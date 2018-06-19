1472cf1c26aaac66496e8cd87937d252954b6dca
dontito <francistito07@gmail.com>
2018-06-19 10:18:50 +0300

    first update

---
+++ resources/views/pages/skills.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="jumbotron">
+  <h1>Hello, world!</h1>
+  <p>...</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+  </div>
+<!--pactical-introduction-->
+  <div class="practical-introduction">
+    <div class="row">
+      <div class="col-md-4">
+<a href="" style="font-size:1.4em; text-decoration:none;">Fields Search Tip</a>
+<div class="thumbnail">
+  <img src="image/Swisscom.jpg" alt="..." style="height:200px;">
+  <div class="caption">
+    <a href="" style="font-size:1.2em; text-decoration:none;">Tips for New College Grads Who Are Nervous About Landing a Field training</a>
+    <p style="margin-top:10px;">Does the thought of securing your first “real” job put the same pit in your stomach as a calculus final? Employ these strategies for new college grads to reduce anxiety and start your career journey with confidence.</p>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4">
+  <a href="" style="font-size:1.4em; text-decoration:none;">How to be  freelancer</a>
+  <div class="thumbnail">
+  <img src="image/Swisscom.jpg" alt="..." style="height:200px;">
+  <div class="caption">
+    <a href="" style="font-size:1.2em; text-decoration:none;">
+Did You Pay Taxes? Over One-Third of Freelancers Didn’t
+</a>
+    <p style="margin-top:10px;">Does the thought of securing your first “real” job put the same pit in your stomach as a calculus final? Employ these strategies for new college grads to reduce anxiety and start your career journey with confidence.</p>
+  </div>
+  </div>
+      </div>
+      <div class="col-md-4">
+  <a href="" style="font-size:1.4em; text-decoration:none;">Work at Home</a>
+  <div class="thumbnail">
+  <img src="image/Swisscom.jpg" alt="..." style="height:200px;">
+  <div class="caption">
+    <a href="" style="font-size:1.2em; text-decoration:none;">Tips for New College Grads Who Are Nervous About Landing a Field training</a>
+    <p style="margin-top:10px;">Does the thought of securing your first “real” job put the same pit in your stomach as a calculus final? Employ these strategies for new college grads to reduce anxiety and start your career journey with confidence.</p>
+  </div>
+  </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+  <a href="" style="font-size:1.4em; text-decoration:none;">Fields Search Tip</a>
+  <div class="thumbnail">
+  <img src="image/Swisscom.jpg" alt="..." style="height:200px;">
+  <div class="caption">
+    <a href="" style="font-size:1.2em; text-decoration:none;">Tips for New College Grads Who Are Nervous About Landing a Field training</a>
+    <p style="margin-top:10px;">Does the thought of securing your first “real” job put the same pit in your stomach as a calculus final? Employ these strategies for new college grads to reduce anxiety and start your career journey with confidence.</p>
+  </div>
+  </div>
+      </div>
+      <div class="col-md-4">
+  <a href="" style="font-size:1.4em; text-decoration:none;">Fields Search Tip</a>
+  <div class="thumbnail">
+  <img src="image/Swisscom.jpg" alt="..." style="height:200px;">
+  <div class="caption">
+    <a href="" style="font-size:1.2em; text-decoration:none;">Tips for New College Grads Who Are Nervous About Landing a Field training</a>
+    <p style="margin-top:10px;">Does the thought of securing your first “real” job put the same pit in your stomach as a calculus final? Employ these strategies for new college grads to reduce anxiety and start your career journey with confidence.</p>
+  </div>
+  </div>
+      </div>
+      <div class="col-md-4">
+  <a href="" style="font-size:1.4em; text-decoration:none;">Fields Search Tip</a>
+  <div class="thumbnail">
+  <img src="image/Swisscom.jpg" alt="..." style="height:200px;">
+  <div class="caption">
+    <a href="" style="font-size:1.2em; text-decoration:none;">Tips for New College Grads Who Are Nervous About Landing a Field training</a>
+    <p style="margin-top:10px;">Does the thought of securing your first “real” job put the same pit in your stomach as a calculus final? Employ these strategies for new college grads to reduce anxiety and start your career journey with confidence.</p>
+  </div>
+  </div>
+      </div>
+
+       <p style="margin-left:500px; box-shadow:0 4px 4px;color: #fff;"><a href="#" class="btn btn-primary" role="button" >More posts</a> <a href="#top" class="btn btn-default" role="button">Back to Top</a></p>
+    </div>
+  </div><!--end of practical introduction -->
+
+
+<div class="row" style="margin-top:80px; background-color:whitesmoke;">
+
+  </div>
+
+
+</div>
+
+
+</div><!--end of container-->
+@endsection
